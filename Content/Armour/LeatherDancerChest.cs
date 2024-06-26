b84c8e7714e2d4cdfedc6c7b06624dd3d4d6c04a
Jayce <anxjestico@gmail.com>
2024-06-26 23:38:02 +0100

    extra armour pieces + set bonus (not permanent)

---
+++ Content/Armour/LeatherDancerChest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace DustDancers.Content.Armour
+{
+    [AutoloadEquip(EquipType.Body)]
+    public class LeatherDancerChest : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.defense = 12;
+            Item.value = 1000;
+            Item.rare = ItemRarityID.Green;
+        }
+
+        public override void UpdateEquip(Player player)
+        {
+            player.GetDamage(DamageClass.Magic) += 0.05f;
+        }
+
+        public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe();
+            recipe.AddIngredient(ItemID.IronBar, 15);
+            recipe.Register();
+        }
+    }
+}
---
+++ Content/Armour/LeatherDancerHead.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace DustDancers.Content.Armour
+{
+    [AutoloadEquip(EquipType.Head)]
+    public class LeatherDancerHead : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.defense = 4;
+            Item.value = 1000;
+            Item.rare = ItemRarityID.Green;
+        }
+
+        public override void UpdateEquip(Player player)
+        {
+            player.GetDamage(DamageClass.Magic) += 0.05f;
+        }
+
+        public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe();
+            recipe.AddIngredient(ItemID.IronBar, 15);
+            recipe.Register();
+        }
+
+        public override void UpdateArmorSet(Player player)
+        {
+            player.setBonus = 
+                "\n+25% movement speed\n" +
+                "+10% acceleration\n" +
+                "+12% mining speed\n" +
+                "'To be as fast as lightning is to be a third as fast of what's preferred' - Adahn Rose";
+            player.moveSpeed += 0.25f;
+            player.runAcceleration += 0.10f;
+            player.pickSpeed -= 0.12f;
+        }
+
+        public override bool IsArmorSet(Item head, Item body, Item legs)
+        {
+            if (body.type == ModContent.ItemType<LeatherDancerChest>() && legs.type == ModContent.ItemType<LeatherDancerLegs>()) 
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
